a18e277812d369b8ccef1bc9378e3be381990b00
rjmfernandes <rjmfernandes@gmail.com>
2023-04-16 00:59:35 +0200

    minimal reffactoring of click message for open thread custom script'

--- click-message-thread/custom-login.js
+++ click-message-thread/custom-login.js
@@ -3,19 +3,22 @@ let observer = new MutationObserver((mutations) => {
       let oldValue = mutation.oldValue;
       let newValue = mutation.target.textContent;
       if (oldValue !== newValue) {
-        for(var i=0;i<document.getElementsByClassName('rcx-message').length;i++){
-          var el=document.getElementsByClassName('rcx-message')[i];
-          if(el.getElementsByClassName('rcx-box rcx-box--full rcx-box--animated rcx-button--small rcx-button--primary rcx-button').length!=0&&el.getAttribute('listener') !== 'true'){
-            el.addEventListener('click',function () {this.getElementsByClassName('rcx-box rcx-box--full rcx-box--animated rcx-button--small rcx-button--primary rcx-button')[0].click();},false);
+        let messages = document.querySelectorAll('.rcx-message');
+        messages.forEach(el => {
+          if (!el.getAttribute('listener') && el.querySelector('.rcx-box.rcx-box--full.rcx-box--animated.rcx-button--small.rcx-button--primary.rcx-button')) {
+            el.addEventListener('click', function () {
+              this.querySelector('.rcx-box.rcx-box--full.rcx-box--animated.rcx-button--small.rcx-button--primary.rcx-button').click();
+            }, false);
+            el.setAttribute('listener', 'true');
           }
-        }
+        });
       }
     });
   });
   
   observer.observe(document.body, {
-    characterDataOldValue: true, 
-    subtree: true, 
-    childList: true, 
+    characterDataOldValue: true,
+    subtree: true,
+    childList: true,
     characterData: true
   });
