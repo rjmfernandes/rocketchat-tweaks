38974350a9eb34e6cecc1c8278713ba87d01c6c7
rjmfernandes <rjmfernandes@gmail.com>
2023-04-16 23:54:53 +0200

    restrict the elements divs in which to add the click behaviour to monimize conflict with other controls

--- click-message-thread/custom-login.js
+++ click-message-thread/custom-login.js
@@ -3,7 +3,7 @@ let observer = new MutationObserver((mutations) => {
       let oldValue = mutation.oldValue;
       let newValue = mutation.target.textContent;
       if (oldValue !== newValue) {
-        let messages = document.querySelectorAll('.rcx-message');
+        let messages = document.querySelectorAll('.rcx-box.rcx-box--full.rcx-message-container');
         messages.forEach(el => {
           if (!el.getAttribute('listener') && el.querySelector('.rcx-box.rcx-box--full.rcx-box--animated.rcx-button--small.rcx-button--primary.rcx-button')) {
             el.addEventListener('click', function () {
