8d5f1dd61eb937b07ddb09b9371855d3dd1dd573
rjmfernandes <rjmfernandes@gmail.com>
2023-04-17 17:43:04 +0200

    adding comments to script code

--- click-message-thread/custom-login.js
+++ click-message-thread/custom-login.js
@@ -3,9 +3,12 @@ let observer = new MutationObserver((mutations) => {
       let oldValue = mutation.oldValue;
       let newValue = mutation.target.textContent;
       if (oldValue !== newValue) {
+        //get message parent divs to add the behaviour
         let messages = document.querySelectorAll('.rcx-box.rcx-box--full.rcx-message-container');
         messages.forEach(el => {
+          //check if message element has no click listener set and contains a reply button
           if (!el.getAttribute('listener') && el.querySelector('.rcx-box.rcx-box--full.rcx-box--animated.rcx-button--small.rcx-button--primary.rcx-button')) {
+            //make the click on message element the same as clicking the inner reply button
             el.addEventListener('click', function () {
               this.querySelector('.rcx-box.rcx-box--full.rcx-box--animated.rcx-button--small.rcx-button--primary.rcx-button').click();
             }, false);
