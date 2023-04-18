effcbdb0472e0bfbde9e33cbc5acffa3f2e92cff
rjmfernandes <rjmfernandes@gmail.com>
2023-04-18 19:46:08 +0200

    just format

--- omnichannel-alert/custom-login.js
+++ omnichannel-alert/custom-login.js
@@ -5,10 +5,10 @@ const warning_message = ' 🔺';
 const consoleDebug = true;
 
 function warningOnChats() {
-  if(consoleDebug)
+  if (consoleDebug)
     console.log('executing now warning on chats');
   let nowDate = new Date();
-  if(consoleDebug)
+  if (consoleDebug)
     console.log('now=' + nowDate.toLocaleString());
   //get chats
   let chats = document.querySelectorAll('.rc-box.rcx-box--full.rcx-sidebar-item.rcx-sidebar-item--clickable');
@@ -16,12 +16,12 @@ function warningOnChats() {
     //if it has an href corresponding to omnichannel chats
     if (chat.getAttribute("href") && chat.getAttribute("href").indexOf('/live') === 0) {
       let chatDate = new Date(chat.getAttribute('time'));
-      if(consoleDebug)
+      if (consoleDebug)
         console.log('chat date=' + chatDate.toLocaleString());
       let diffSs = (nowDate.getTime() - chatDate.getTime()) / 1000;//seconds
-      if(consoleDebug)
+      if (consoleDebug)
         console.log('difference=' + diffSs);
-      if(consoleDebug)
+      if (consoleDebug)
         console.log(maxSeconds);
       //get the inner div with label for chat
       let testBox = chat.querySelector('.rc-box.rcx-box--full.rcx-sidebar-item__title');
@@ -31,7 +31,7 @@ function warningOnChats() {
         //we store old label under warned and include on label the warning message
         chat.setAttribute('warned', testBox.innerHTML);
         testBox.innerHTML = testBox.innerHTML + warning_message;
-      //else if the inner div exists and already marked as warned and time since last update less than maxSeconds
+        //else if the inner div exists and already marked as warned and time since last update less than maxSeconds
       } else if (testBox && chat.getAttribute('warned') && chat.getAttribute('warned') !== 'false' && diffSs < maxSeconds) {
         //we set warned as false and use the value stored on it as the new label
         testBox.innerHTML = chat.getAttribute('warned');
